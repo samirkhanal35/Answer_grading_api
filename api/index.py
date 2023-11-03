702ad1c6e4c5db048e0601d356231cf241891928
samirkhanal35 <samirkhanal35@gmail.com>
2023-11-03 19:18:48 -0400

    testing 2

--- api/index.py
+++ api/index.py
@@ -8,11 +8,5 @@ def hello():
     return 'Hello, world'
 
 
-@app.route('/test')
-def test():
-    return 'Test'
 
-@app.route('/result')
-def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
+
